280c1f705100caa720015a454354fd62d78cb73c
Poornima Ranganath <pranganath@mvista.com>
2014-09-02 13:14:32 +0530

    i2c: Fix i2c memory corruption due to race conditions
    The patch fixes race conditions in i2c driver code by
    moving spin_unlock_irqrestore in i2c_perform_op_wait(),
    using ret, not entry->result, in i2c_perform_op_wait(),
    moving next_entry in i2c_op_done().
    
    Signed-off-by: Poornima Ranganath <pranganath@mvista.com>

--- drivers/i2c/i2c-core.c
+++ drivers/i2c/i2c-core.c
@@ -1874,10 +1874,11 @@ static void i2c_perform_op_wait(struct i2c_adapter *adap,
 			entry->result = ret;
 			list_del(&entry->link);
 		}
-		spin_unlock_irqrestore(&adap->q_lock, flags);
 	}
 
-	if (!entry->result)
+	spin_unlock_irqrestore(&adap->q_lock, flags);
+
+	if (!ret)
 		wait_for_completion(&entry->done);
 }
 
@@ -2895,10 +2896,10 @@ void i2c_op_done(struct i2c_adapter *adap, struct i2c_op_q_entry *entry)
 		}
 	}
 
+next_entry:
 	entry->state = I2C_OP_FINISHED;
 	list_del(&entry->link);
 
-next_entry:
 	spin_unlock_irqrestore(&adap->q_lock, flags);
 
 	if (entry->complete)
