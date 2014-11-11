06f48762e2cb1dc20ed45739599daa1b71478cee
Corey Minyard <cminyard@mvista.com>
2014-11-10 21:10:49 -0600

    i2c: Add parameters to sysfs-added i2c devices
    Some devices might need parameters to control their operation,
    add the ability to pass these parameters to the client.
    
    This also makes the parsing of sysfs-added I2C devices a little
    more flexible, allowing tabs and arbitrary numbers of spaces.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -243,6 +243,7 @@ struct i2c_client {
 	struct i2c_adapter *adapter;	/* the adapter we sit on	*/
 	struct i2c_driver *driver;	/* and our access routines	*/
 	struct device dev;		/* the device structure		*/
+	char *parms;			/* sysfs extra parms		*/
 	int irq;			/* irq issued by device		*/
 	struct list_head detected;
 };
@@ -276,6 +277,7 @@ static inline void i2c_set_clientdata(struct i2c_client *dev, void *data)
  * @archdata: copied into i2c_client.dev.archdata
  * @of_node: pointer to OpenFirmware device node
  * @acpi_node: ACPI device node
+ * @parms: Parameters supplied on the sysfs command line
  * @irq: stored in i2c_client.irq
  *
  * I2C doesn't actually support hardware probing, although controllers and
@@ -297,6 +299,7 @@ struct i2c_board_info {
 	struct dev_archdata	*archdata;
 	struct device_node *of_node;
 	struct acpi_dev_node acpi_node;
+	char            *parms;
 	int		irq;
 };
 
