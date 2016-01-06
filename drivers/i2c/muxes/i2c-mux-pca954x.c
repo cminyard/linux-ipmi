e47086d217435b5df0f1a81005fb72be1ad08d0b
Corey Minyard <cminyard@mvista.com>
2016-01-06 13:44:08 -0600

    i2c-mux-pca954x: Fix non-delayed select
    The wrong i2c command was being used.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- drivers/i2c/muxes/i2c-mux-pca954x.c
+++ drivers/i2c/muxes/i2c-mux-pca954x.c
@@ -130,7 +130,7 @@ static int pca954x_reg_write(struct i2c_adapter *adap,
 {
 	return i2c_smbus_xfer_nolock(adap, client->addr, client->flags,
 				     I2C_SMBUS_WRITE, val,
-				     I2C_SMBUS_BYTE_DATA, NULL);
+				     I2C_SMBUS_BYTE, NULL);
 }
 
 static int pca954x_d_reg_write(struct pca954x *data, struct i2c_adapter *adap,
